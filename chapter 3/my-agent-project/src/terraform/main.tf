24b1209e406017afefa8930eb9f53c3b338789ed
AKANKSHA SRIVASTAVA <lovely.girl.28022006@gmail.com>
2025-12-05 00:21:54 +0530

    Create main.tf

---
+++ chapter 3/my-agent-project/src/terraform/main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_ecs_cluster" "agent_cluster" {
+  name = "agent-cluster"
+  
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+}
+
+resource "aws_ecs_task_definition" "agent_task" {
+  family                   = "agent-app"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name  = "agent-app"
+      image = "ghcr.io/your-org/my-agent-project:latest"
+      
+      portMappings = [
+        {
+          containerPort = 8000
+          protocol      = "tcp"
+        }
+      ]
+      
+      environment = [
+        {
+          name  = "ENVIRONMENT"
+          value = "production"
+        }
+      ]
+      
+      secrets = [
+        {
+          name      = "OPENAI_API_KEY"
+          valueFrom = aws_secretsmanager_secret.openai_key.arn
+        }
+      ]
+    }
+  ])
+}
